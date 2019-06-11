3124ddf4dd6a952b8594ec903c85cf6411b7e659
AlutkaG <50596853+AlutkaG@users.noreply.github.com>
2019-06-11 22:34:07 +0200

    Create listaSasiedztwa.h

---
+++ listaSasiedztwa.h
@@ -0,0 +1,11 @@
+#ifndef listaSasiedztwa_h
+#define listaSasiedztwa_h
+#include <iostream>
+
+class ListaSasiedztwa {
+public:
+	int v, w; //wierzchołek poczatkowy i waga krawędzi
+	ListaSasiedztwa* nastepny;
+};
+
+#endif
